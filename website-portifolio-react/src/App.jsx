8ae9f18bb4c0075dc9c322fed977127bbc3f07dd
Luiz Felipe Rodrigues de Melo <mail.lfellipe@gmail.com>
2024-11-26 16:05:08 -0300

    commit 26/11

--- website-portifolio-react/src/App.jsx
+++ website-portifolio-react/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import BackgroundImageComponent from './components/BackgroundImageComponent';
 import HomebarComponent from './components/HomebarComponent';
 
 function App() {
   return (
     <div className="App">
       <HomebarComponent />
+      <div className='background-image'>
+      </div>
+      
+
     </div>
   );
 }
---
+++ website-portifolio-react/src/components/BackgroundImageComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BackgroundImage from "../images/backgroundimage.png";
+
+const BackgroundImageComponent = () => {
+  return (
+    <div>
+        <img src={BackgroundImage} alt="" />
+    </div>
+  );
+}
+
+export default BackgroundImageComponent;
