d36129f4fe70df3e0386d54799909be046e4bb4b
Luiz Felipe Rodrigues de Melo <mail.lfellipe@gmail.com>
2024-11-26 22:45:37 -0300

    commit build 1.0.1.0-PUBLICDTB

--- website-portifolio-react/src/components/HomebarComponent.jsx
+++ website-portifolio-react/src/components/HomebarComponent.jsx
@@ -15,7 +15,7 @@ const HomebarComponent = () => {
           <ul className='homebar-main'>
             <li className='homebar-logo'><img src={lfellipestudios} alt="" width={370} /></li>
             <div className='homebar-items'>
-            <li className='homebar-item'><a href="Inicio">Início</a></li>
+            <li className='homebar-item' id='homebarInicio'><a href="Inicio">Início</a></li>
             <li className='homebar-item'><a href="Sobre">Sobre</a></li>
             <li className='homebar-item'><a href="Portifolio">Portifólio</a></li>
             <li className='homebar-item'><a href="Contato">Contato</a></li>
